519014c24fad8c2f1fdd982c8016470f115477b1
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-09-06 11:11:43 +0200

    create at-site frequency analysis script

---
+++ code/scripts/model-fitting/at-site-freq-analysis-with-stan.R
@@ -0,0 +1,17 @@
+##
+##
+##
+##
+##
+## Response variables for the GAM models are GEV parameters obtained from 
+## at-site frequency analysis.
+##
+## This script fits a GEV distribution at each gamfelt station.
+## -----------------------------------------------------------------------------
+
+
+
+
+
+
+
