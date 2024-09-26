da6d17b6d2ba171f7cf19295941df1735af0d231
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-09-26 15:23:20 +0200

    find which nifs stations we do not already have

--- code/scripts/data-creation/streamflow/NIFS-create-database-commands.R
+++ code/scripts/data-creation/streamflow/NIFS-create-database-commands.R
@@ -16,7 +16,31 @@ library(stringr)
 nifs.tab <- fread("~/floodGAM/data/raw-data/NIFS_stations_table_5.txt",
                   fill=T, colClasses = list(character=1:20))
 
-## first column is station numbers 
+## first column is station numbers. We want the station numbers to create
+## the lescon_var executable:
+nifs.tab[, c("RN", "HN") := tstrsplit(V1, ".", fixed=TRUE)]
+
+
+## cross-check with Table A2. How many of these stations do we already have?
+tabellA2 <- data.table::fread(paste0("~/floodGAM/data/raw-data/",
+                                     "table_A2_report_2016-85.csv"),
+                              colClasses = list(character=1))
+tabellA2[, c("RN", "HN") := tstrsplit(Nr, ".", fixed=TRUE)]
+
+
+iA2 <- merge(tabellA2[,c("RN","HN")],nifs.tab[,c("RN","HN")],by=c("RN","HN"))
+dim(iA2)
+# so we already have 95 of the nifs stations in the original lescon_var call.
+
+setkey(iA2,RN,HN)
+setkey(nifs.tab,RN,HN)
+
+# these are the stations we do not already call:
+these <- nifs.tab[!iA2]
+
+
+
+
 
 
 
