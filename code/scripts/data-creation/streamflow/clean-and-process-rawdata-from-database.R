28f824f349d8fe5da23e1c6f2b3252b32b154c57
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-10-18 16:22:18 +0200

    the big update: old database to new

--- code/scripts/data-creation/streamflow/clean-and-process-rawdata-from-database.R
+++ code/scripts/data-creation/streamflow/clean-and-process-rawdata-from-database.R
@@ -15,7 +15,7 @@ library(data.table)
 library(lubridate)
 
 ## set the path to the directory you downloaded the data files into
-path <- "C:/lescon_data"
+path <- "C:/new_lescon_data"
 setwd(path) 
 
 ## list all .txt files in the directory. Each individual .txt file 
@@ -23,12 +23,20 @@ setwd(path)
 ## to get all data into a single data.table
 alltxtfiles <- list.files(pattern = ".txt") 
 
-## differentiate between files from arkiv 5 and arkiv 35:
-fl05 <- list.files(pattern = "5_")
-fl35 <- alltxtfiles[-which(alltxtfiles %in% fl05)]
-fl35 <- fl35[-which(grepl("lescon", fl35))]
+## check for any empty files:
+## (if file is empty, check lescon_var command..
+## they should all work as of 18.10.2024)
+idx <- which(file.info(list.files(".", 
+                                  all.files = TRUE, 
+                                  recursive = TRUE))$size == 0)
+alltxtfiles[idx]
 
-flList <- list(fl05,fl35)
+## differentiate between files from arkiv 5 and arkiv 35:
+fl37 <- list.files(pattern = "37_")
+fl39 <- alltxtfiles[-which(alltxtfiles %in% fl37)]
+fl39 <- fl39[-which(grepl("lescon", fl39))]
+
+flList <- list(fl37,fl39)
 
 for(i in 1:2){ # loop over the archives
   
@@ -61,15 +69,15 @@ for(i in 1:2){ # loop over the archives
                                             ("date"):=lapply(.SD,ymd_hms,tz="UTC"),
                                             .SDcols = "date"]
              })
-    n  <- ifelse(i == 1, 3, 1)
+    n  <- ifelse(i == 1, 4, 1)
     station.data[,ID:=substr(thisfile,n,nchar(thisfile)-4)]
     data <- rbind(data,station.data)
   } # end file loop
   
   ## save the data file somewhere (define "dataPath")
   if(i == 1){
-    saveRDS(data, paste0(dataPath,"arkiv05","data.rds"))
+    saveRDS(data, paste0("~/floodGAM/data/raw-data/","arkiv37","data.rds"))
   }else{
-    saveRDS(data, paste0(dataPath,"arkiv35","data.rds"))
+    saveRDS(data, paste0("~/floodGAM/data/raw-data/","arkiv39","data.rds"))
   }
 } # end archive loop
--- code/scripts/data-creation/streamflow/create-database-commands.R
+++ code/scripts/data-creation/streamflow/create-database-commands.R
@@ -5,9 +5,8 @@
 ##
 ## based on table A2 from NVE report 2016:85 (data for flood freq analysis)
 ##
-## takes the station names from table A2
-## and version numbers from a mix of Kolbjørn's script
-## and manual control (i.e. checking which version actually exists)
+## takes the station names from table A2.
+## a few version numbers are manually controlled
 ##
 ## ---------------------------------------------
 
@@ -17,72 +16,62 @@ library(stringr)
 ## set a working directory. R will write the text file to this directory.
 setwd("~/floodGAM/data/raw-data/")
 
-## ------- Les inn data: 
-## read in the manually controlled version numbers
-altversions <- data.table::fread(paste0("~/floodGAM/data/raw-data/",
-                              "alternate_version_numbers.csv"))
 ## read in table A2 from report 2016:85
 tabellA2 <- data.table::fread(paste0("~/floodGAM/data/raw-data/",
-                                     "table_A2_report_2016-85.csv"))
+                                     "tabell_for_rapport2016_forenkelt_v.txt"),
+                              colClasses = list(character=1))
 
-
+## first column is station numbers. We want the station numbers to create
+## the lescon_var executable:
+tabellA2[, c("RN", "HN") := tstrsplit(Nr, ".", fixed=TRUE)]
 
 ## there are 530 stations in table A2. Some of these stations have just daily 
 ## data, some have both daily data and findata.
 dim(tabellA2)[1]
 
 ## we exclude the stations with only daily data. 
-## This leaves 329 stations with at least some findata:
+## This leaves 327 stations with at least some findata:
 findata <- tabellA2[is.na(Findata_N) != T] 
 dim(findata)[1]
 
-## ------- Stasjon nummer fra Tabell A2 samsvarer ikke med navnekonvensjonen 
-## som brukes i lescon_var. Vi fikser dette:
-
-zeroInd <- findata[, .I[which(onezero==T)]] # append 1 to fix trailing zeros
-findata[zeroInd, Nr:=(Nr+1*10^(-(numdig+1)))]
-findata[, Nr:=as.character(Nr)]
-findata[, c("RN", "HN") := tstrsplit(Nr, ".", fixed=TRUE)]
-findata[zeroInd, HN:=substr(HN,1,(nchar(HN)-1))] # get rid of the 1
+## create ID column (this format matches an old version of a covariate file;
+## now it just functions as a key column)
 findata[,HNpad:=lapply(.SD,str_pad,width=5,pad="0"),.SDcols="HN"]
 findata[,ID:=paste0(RN,HNpad)]
 
 Nrlescon <- findata[,c("RN", "HN", "version","ID")]
 
-# stitch together rn and hn and pad with zeros to make
-# names to call files from server
+## these stations need different version numbers than the ones
+## listed in Table A2 2016:85
+changevnum <- data.table(ID=c("200102","1200113","7100001",
+                              "7200005","10300003",
+                              "11100001","12700006",
+                              "1100004"),
+                         version=c(0,2,2,2,2,3,2,2))
+setkey(changevnum,ID)
+setkey(Nrlescon,ID)
+
+Nrlescon[.(changevnum),version := changevnum$version]
+
+## make file name column:
 Nrlescon[,flnm:= paste0(ID,".txt")]
 
 
-## ------- Legg til manuel-kontrollert versjonsnumre:
-  
-# now add in alternate version numbers from Kolbjørn provided script
-altversions[,ID:=lapply(.SD,function(x) substr(x,1,nchar(x)-4)),
-            .SDcols = "ID"]
-
-Nrlescon <- merge(Nrlescon,altversions,all.x=T)
-
-# in situations where kvnum doesn't exist fill with "version" (the manually 
-# corrected version)
-idx <- Nrlescon[,.(idx = .I[is.na(kvnum)])]$idx
-Nrlescon$kvnum[idx] <- Nrlescon$version[idx]
-
-
 ## ------- Skriv den kjørebare lescon_var filen:
-  
+
 ## good idea to double check the working directory
 ## before opening the connection to bash
 
-## name the executable lescon_var file (here we name it 
-## "my_lescon_var_commands.txt") and open the connection
-f <- file("lescon_var_commands_arkiv_37_gamfelt.txt", open="wb")
+## archive 39 (hyfin completed)
+## name the executable lescon_var file and open the connection
+f <- file("lescon_var_commands_archive_39.txt", open="wb")
 
 ## write text to file using cat command
 cat("# !/bin/bash",file=f,append=F,sep="\n")
 for(i in 1:dim(Nrlescon)[1]){
-  cat(paste0("lescon_var -b 0 -f timevalue 37 ",
+  cat(paste0("lescon_var -b 0 -f timevalue 39 ",
              Nrlescon$RN[i], " ", Nrlescon$HN[i],
-             " 0 1001 ",Nrlescon$kvnum[i]," > ",
+             " 0 1001 ",Nrlescon$version[i]," > ",
              Nrlescon$flnm[i]),
       file=f,append=T,sep="\n")
 }
@@ -90,7 +79,21 @@ for(i in 1:dim(Nrlescon)[1]){
 ## close the connection
 close(f)
 
-## the my_lescon_var_commands.txt file should now be found in the 
-## working directory
+
+## archive 37 (hydag)
+f <- file("lescon_var_commands_archive_37.txt", open="wb")
+
+## write text to file using cat command
+cat("# !/bin/bash",file=f,append=F,sep="\n")
+for(i in 1:dim(Nrlescon)[1]){
+  cat(paste0("lescon_var -b 0 -f timevalue 37 ",
+             Nrlescon$RN[i], " ", Nrlescon$HN[i],
+             " 0 1001 ",Nrlescon$version[i]," > ",
+             "37_",Nrlescon$flnm[i]),
+      file=f,append=T,sep="\n")
+}
+
+## close the connection
+close(f)
 
 
