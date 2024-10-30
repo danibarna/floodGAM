4d5e13b7f191d7f04aa6b0b9a1152aaccee10f1c
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-10-30 16:15:50 +0100

    add some predictive accuracy files

---
+++ code/scripts/model-evaluation/median-(index-flood)/predictive_accuracy.R
@@ -0,0 +1,85 @@
+##
+##
+##
+##
+##
+## Evaluate fitted models on:
+## - predictive accuracy at oos locations (5 metrics)
+## - duration consistency
+## - reliability (PIT)
+## -----------------------------------------------------------------------------
+
+library(data.table)
+library(ggplot2)
+library(ggpubr)
+library(scico)
+
+## source function permutation test:
+source("~/floodGAM/code/functions/fn_calc_optimal_predictor.R")
+## source functions to quickly plot and check results:
+source("~/floodGAM/code/functions/fn_check_results_with_plots.R")
+
+## ----- load in the predictions and error metrics:
+oos.pred <- readRDS(paste0("~/floodGAM/results/output/median-(index-flood)/",
+                           "median-index-flood-predictive-accuracy.rds"))
+
+## ----- for plotting of evaluation metrics:
+gfcov <- readRDS(paste0("~/floodGAM/data/processed-data/gamfelt/",
+                        "gamfelt_catchment_covariates.rds"))
+
+
+# Predictive accuracy -----------------------------------------------------
+
+scre <- oos.pred[,
+                 lapply(.SD,mean),.SDcols = c("crps","ae","re","ape"),
+                 by=c("model","d")]
+
+scre <- merge(scre,
+              oos.pred[,sqrt(mean(se)),by=c("model","d")],
+              by=c("model","d"))
+
+setnames(scre,"V1","rmse")
+
+scre[,re:=re*100]
+scre[,ape:=ape*100]
+
+scre[,c("rmse","crps","ae","re","ape") := lapply(.SD,round,1),
+     .SDcols= c("rmse","crps","ae","re","ape")]
+
+setkey(scre,d)
+
+## TABLE 4 - error metrics
+scre[,c("model","d","rmse","crps","ae","re","ape")]
+
+## statistical significance of different error metrics:
+for(di in unique(oos.pred$d)){
+  print(paste0(di," - ",permutationTest(oos.pred,
+                                        "floodGAM","RFFA2018",
+                                        1000,
+                                        "se",di)) )
+}
+
+
+## ------- Dotplots - predictive accuracy
+
+oos.pred <- merge(oos.pred,gfcov[,c("ID","A","QD_fgp")],by="ID")
+
+glist <- list()
+i = 1
+for(di in unique(oos.pred$d)){
+  
+  ggdat <- dcast(oos.pred[d==di], ID + A + QD_fgp ~ model, value.var = "se")
+  
+  glist[[i]] <- dotplotRFFA2018floodGAM(ggdat,0,500000,paste0("SE, d = ",di))
+  
+  i = i+1
+}
+
+figure <- ggarrange(glist[[1]],glist[[2]],glist[[3]],glist[[4]],
+                    glist[[5]],glist[[6]],glist[[7]],glist[[8]],
+                    nrow=2,ncol = 4,
+                    common.legend = T, legend = "bottom")
+
+figure
+
+glist[[6]]
