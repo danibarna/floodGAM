0d9f3456ac2d40d3c378154526bd28dc3a9aa0ef
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-11-15 10:17:52 +0100

    comparison script for stations used in 2013, 2015 and 2016 reports

---
+++ code/scripts/2-data-exploration/compare_stations_NIFS-SMA-RFFA.R
@@ -0,0 +1,63 @@
+##
+##
+##
+##
+##
+## make table comparing stations from:
+## - små vassdrags rapport https://publikasjoner.nve.no/rapport/2013/rapport2013_66.pdf
+## - NIFS https://publikasjoner.nve.no/rapport/2015/rapport2015_13.pdf
+## - RFFA https://publikasjoner.nve.no/rapport/2016/rapport2016_85.pdf
+##
+## -----------------------------------------------------------------------------
+
+library(data.table)
+library(lubridate)
+
+
+nifs.tab <- fread("~/floodGAM/data/raw-data/NIFS_stations_table_5.txt",
+                  fill=T, colClasses = list(character=1))
+nifs.tab[, c("RN", "HN") := tstrsplit(V1, ".", fixed=TRUE)]
+setnames(nifs.tab,"V2","navn")
+nifs.tab <- nifs.tab[,c("navn","RN","HN")]
+nifs.tab[,NIFS:="nifs"]
+
+A2.tab <- fread("~/floodGAM/data/raw-data/tabell_for_rapport2016_forenkelt_v.txt",
+                fill=T, colClasses = list(character=1))
+A2.tab[, c("RN", "HN") := tstrsplit(Nr, ".", fixed=TRUE)]
+A2.tab[,A2:="A2"]
+
+
+A3.tab <- fread("~/floodGAM/data/raw-data/rapport2016_85_tabell_A3_list_over_utelatt_fra_flomdatasett.txt",
+                fill=T, colClasses = list(character=1:2))
+A3.tab[, key_ := do.call(paste, c(.SD, sep = " ")), .SDcols = names(A3.tab)]
+A3.tab <- A3.tab[,c("V1","key_")]
+A3.tab[, c("RN", "HN") := tstrsplit(V1, ".", fixed=TRUE)]
+A3.tab[,A3:="A3"]
+A3.tab <- A3.tab[,c("key_","RN","HN","A3")]
+
+
+
+sma.tab <- fread("~/floodGAM/data/raw-data/rapport2013_66_tabell_1.txt",
+                 fill=T, colClasses = list(character=1))
+sma.tab[,c("RN","HN") := tstrsplit( 
+  sma.tab[, tstrsplit(V1, " ", fixed=TRUE)]$V1, ".", fixed=TRUE)]
+sma.tab <- sma.tab[,c("RN","HN")]
+sma.tab[,SMA:="sma"]
+
+setkey(nifs.tab,RN,HN); setkey(A2.tab,RN,HN)
+setkey(A3.tab,RN,HN); setkey(sma.tab,RN,HN)
+
+
+# in which report? --------------------------------------------------------
+
+tab <- merge(nifs.tab[,c("RN","HN","navn","NIFS")],
+             A2.tab[,c("RN","HN","A2")],all=T)
+
+tab <- merge(tab,sma.tab[,c("RN","HN","SMA")],all=T)
+
+
+tab <- merge(tab,A3.tab[,c("RN","HN","A3","key_")],all=T)
+
+# create lescon_var commands ----------------------------------------------
+
+
