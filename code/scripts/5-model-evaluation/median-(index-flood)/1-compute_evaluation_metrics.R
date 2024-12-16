9fce5c4e27d84bb5604ead96536e66606f942708
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2024-12-16 10:33:50 +0100

    rename to indicate order of operations

--- code/scripts/5-model-evaluation/median-(index-flood)/1-compute_evaluation_metrics.R
+++ code/scripts/5-model-evaluation/median-(index-flood)/1-compute_evaluation_metrics.R
@@ -17,7 +17,7 @@ source("~/floodGAM/code/functions/fn_calc_optimal_predictor.R")
 
 ## ----- load in the predictions from the models:
 oos.pred <- readRDS(paste0("~/floodGAM/results/output/median-(index-flood)/",
-                           "median-index-flood-oos-predictions.rds"))
+                           "gamfelt_median_flood_oos_pred.rds"))
 
 # Predictive accuracy -----------------------------------------------------
 
@@ -45,4 +45,4 @@ oos.pred[,ape:=abs( (eta.obs-eta.ape)/eta.obs )]
 
 saveRDS(oos.pred,
         file = paste0("~/floodGAM/results/output/median-(index-flood)/",
-                      "median-index-flood-predictive-accuracy.rds"))
+                      "gamfelt_median_flood_predictive_accuracy.rds"))
