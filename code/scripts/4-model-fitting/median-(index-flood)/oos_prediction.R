4b677b642839ce336ba93266237b3569db86b918
Danielle M Barna <49793254+danibarna@users.noreply.github.com>
2025-01-31 10:55:38 +0100

    add regions predictions

--- code/scripts/4-model-fitting/median-(index-flood)/oos_prediction.R
+++ code/scripts/4-model-fitting/median-(index-flood)/oos_prediction.R
@@ -51,24 +51,14 @@ gamdat <- merge(gfcov,gfam,by="ID")
 
 
 # Define the data folds ---------------------------------------------------
-# set.seed(8)
-# k = 10
-# # use the 24 hour duration
-# fidx <- createFolds(gamdat[d==24,get("qind")],k) 
+set.seed(8)
+k = 10
+# use the 24 hour duration
+fidx <- createFolds(gamdat[d==24,get("qind")],k)
 
 
-## !!! Start seed loop
-for( thisseed in c(30,32,85)){
-  
-  set.seed(thisseed)
-  k = 10
-  # use the 24 hour duration
-  fidx <- createFolds(gamdat[d==24,get("qind")],k) 
-  
-  
   iis.models <- readRDS(paste0("~/floodGAM/results/output/median-(index-flood)/",
-                               "gamfelt_hydagsupp_featuresFromIIS_gam_",
-                               thisseed,".rds"))
+                               "gamfelt_hydagsupp_featuresFromIIS_gam",".rds"))
   
   iis.models <- iis.models[edf>0.001]
 
@@ -120,6 +110,7 @@ for(di in unique(gfam[,get("d")])){
     eta.floodGAM <- gam(qind ~ s(Q_N,k=6)+s(A_LE,k=6)+s(A_P,k=6)+s(H_F,k=6)+
                           s(log_R_G_1085,k=6)+s(W_Apr,k=3)+s(P_Sep,k=3),
                         method = "REML",
+                        select = T,
                         data = train.gamdat.d,
                         family = gaussian(link=log))
     
@@ -142,11 +133,13 @@ for(di in unique(gfam[,get("d")])){
                            s(A_Agr,k=3)+s(A_For,k=3)+
                            s(P_Apr,k=3)+s(W_Jul,k=3),
                          method = "REML",
+                         select = T,
                          data = train.gamdat.d,
                          family = gaussian(link=log))
     
     eta.auto <- gam(fml,
                     method = "REML",
+                    select = T,
                     data = train.gamdat.d,
                     family = gaussian(link=log))
     
@@ -253,29 +246,29 @@ for(di in unique(gfam[,get("d")])){
                                ID = test.gamdat.d[,get("ID")]))
     
     
-    # ## --- store the simulations from the posterior 
-    # 
-    # ## ------------------ floodGAM
-    # posterior.draws <- rbind(posterior.draws,
-    #                          data.table(
-    #                            eta.draws=simulateFromPosterior(eta.floodGAM,
-    #                                                            "eta",
-    #                                                            test.gamdat.d)$draws,
-    #                            model=rep("floodGAM",n*5000),
-    #                            fold=rep(i,n*5000),
-    #                            d=rep(di,n*5000),
-    #                            ID=rep(test.gamdat.d[,get("ID")],each=5000)))
-    # 
-    # ## ------------------ RFFA2018
-    # posterior.draws <- rbind(posterior.draws,
-    #                          data.table(
-    #                            eta.draws=simulateFromPosterior(eta.RFFA2018,
-    #                                                            "eta",
-    #                                                            test.gamdat.d)$draws,
-    #                            model=rep("RFFA2018",n*5000),
-    #                            fold=rep(i,n*5000),
-    #                            d=rep(di,n*5000),
-    #                            ID=rep(test.gamdat.d[,get("ID")],each=5000)))
+    ## --- store the simulations from the posterior
+
+    ## ------------------ floodGAM
+    posterior.draws <- rbind(posterior.draws,
+                             data.table(
+                               eta.draws=simulateFromPosterior(eta.floodGAM,
+                                                               "eta",
+                                                               test.gamdat.d)$draws,
+                               model=rep("floodGAM",n*5000),
+                               fold=rep(i,n*5000),
+                               d=rep(di,n*5000),
+                               ID=rep(test.gamdat.d[,get("ID")],each=5000)))
+
+    ## ------------------ RFFA2018
+    posterior.draws <- rbind(posterior.draws,
+                             data.table(
+                               eta.draws=simulateFromPosterior(eta.RFFA2018,
+                                                               "eta",
+                                                               test.gamdat.d)$draws,
+                               model=rep("RFFA2018",n*5000),
+                               fold=rep(i,n*5000),
+                               d=rep(di,n*5000),
+                               ID=rep(test.gamdat.d[,get("ID")],each=5000)))
     
   }
 }
@@ -285,37 +278,34 @@ for(di in unique(gfam[,get("d")])){
 
 saveRDS(oos.predictions,
         file = paste0("~/floodGAM/results/output/median-(index-flood)/",
-                           "gamfelt_hydagsupp_median_flood_oos_pred_",thisseed,".rds"))
-# # this one has to be in gitignore because it is too large:
-# saveRDS(posterior.draws,
-#         file = paste0("~/floodGAM/results/output/median-(index-flood)/",
-#                       "gamfelt_hydagsupp_median_index_flood_posterior_draws.rds"))
-
-}
+                           "gamfelt_hydagsupp_median_flood_oos_pred","_T_", ".rds"))
+# this one has to be in gitignore because it is too large:
+saveRDS(posterior.draws,
+        file = paste0("~/floodGAM/results/output/median-(index-flood)/",
+                      "gamfelt_hydagsupp_median_index_flood_posterior_draws.rds"))
 
 
-# Quick stitch to test an alt datadriven GAM ------------------------------
-
-iis.models <- readRDS(paste0("~/floodGAM/results/output/median-(index-flood)/",
-                             "gamfelt_hydagsupp_featuresFromIIS_gam.rds"))
-
-iis.models <- iis.models[edf>0.001]
 
 
-#iis.models[,Feature:=ifelse(Feature=="R_G","log_R_G",Feature)]
 
-# iis.models[,maediff:=c(-100,diff(maeFeat)),by=c("d","fold")]
-# 
-# tt <- iis.models[which(iis.models$maediff>0, arr.ind=TRUE),]
-# 
-# tt<- tt[,min(ordFeat),by=c("d","fold")]
-# 
-# tv <- merge(iis.models,tt,all.x=T,by=c("d","fold"))
-# 
-# tv[,V1 := lapply(.SD, nafill, fill = 50), .SDcols = "V1"]
-# 
-# iis.models.tt <- tv[ordFeat < V1,]
+# Test floodGAM, regions --------------------------------------------------
 
+gamdat[, c("RN", "HN") := tstrsplit(ID, "-", fixed=TRUE)]
+
+gamdat[,RN:=as.numeric(RN)]
+
+
+midsouthwest <- gamdat[(RN>=17&RN<=189)|RN==307|RN==308,]
+
+finmarkeast <- gamdat[(RN<=16|RN>=190)&RN!=307&RN!=308,]
+
+
+## define two different 5 fold cross validations
+set.seed(8)
+k = 5
+# use the 24 hour duration
+fidx.msw <- createFolds(midsouthwest[d==24,get("qind")],k)
+fidx.fme <- createFolds(finmarkeast[d==24,get("qind")],k)
 
 
 oos.predictions <- data.table(eta=numeric(),
@@ -327,58 +317,68 @@ oos.predictions <- data.table(eta=numeric(),
 
 for(di in unique(gfam[,get("d")])){
   
-  gamdat.d <- gamdat[d==di]
+  midsouthwest.d <- midsouthwest[d==di]
+  finmarkeast.d <- finmarkeast[d==di]
   
   for(i in 1:k){
     
-    train.gamdat.d <- gamdat.d[-fidx[[i]]]
-    test.gamdat.d <- gamdat.d[fidx[[i]]]
+    train.midsouthwest.d <- midsouthwest.d[-fidx.msw[[i]]]
+    test.midsouthwest.d <- midsouthwest.d[fidx.msw[[i]]]
     
-    
-    ## auto-data-driven prep
-    stack <- iis.models[d==di&fold==i,get("Feature")]
-    rhs <- paste('s(', stack, ',k=4)', sep = '', collapse = ' + ')
-    # let the first three have 6 degrees of freedom, everything else
-    # gets 3 degrees of freedom:
-    aa <- unlist(gregexpr(pattern ='4',rhs))[1:3]
-    for(j in 1:3){substr(rhs,aa[j],aa[j]) <- "6"}
-    
-    
-    fml <- paste('qind', '~', rhs, collapse = ' ')
-    print(fml)
-    
-    fml <- as.formula(fml)
+    train.finmarkeast.d <- finmarkeast.d[-fidx.fme[[i]]]
+    test.finmarkeast.d <- finmarkeast.d[fidx.fme[[i]]]
     
     
     ## ------- eta --------
     
-    eta.auto <- gam(fml,
-                    method = "REML",
-                         data = train.gamdat.d,
-                         family = gaussian(link=log))
+    eta.midsouthwest <- gam(qind ~ s(Q_N,k=6)+s(A_LE,k=6)+s(A_P,k=6)+s(H_F,k=6)+
+                          s(log_R_G_1085,k=6)+s(W_Apr,k=3)+s(P_Sep,k=3),
+                        method = "REML",
+                        select = T,
+                        data = train.midsouthwest.d,
+                        family = gaussian(link=log))
     
+    eta.finmarkeast <- gam(qind ~ s(Q_N,k=6)+s(A_LE,k=6)+s(A_P,k=6)+s(H_F,k=6)+
+                              s(log_R_G_1085,k=6)+s(W_Apr,k=3)+s(P_Sep,k=3),
+                            method = "REML",
+                            select = T,
+                            data = train.finmarkeast.d,
+                            family = gaussian(link=log))
     
     
     ## ------- generate and save the predictions & predictive uncertainty ------
-    n = dim(test.gamdat.d)[1]
+    n.msw = dim(test.midsouthwest.d)[1]
+    n.fme = dim(test.finmarkeast.d)[1]  
     
     ## --- store the predictions
     
     ## ------------------ floodGAM
-    ## from the GAM: get mu (prediction for the test set on the log scale)
-    mu.gam <- predict(eta.auto,newdata=test.gamdat.d,type="link")
-    ## then estimate standard dev based on in-sample residuals for training data
-    sigma.gam <- sd(log(eta.auto$y) - eta.auto$linear.predictor)
+    mu.gam.msw <- predict(eta.midsouthwest,newdata=test.midsouthwest.d,type="link")
+    sigma.gam.msw <- sd(log(eta.midsouthwest$y) - eta.midsouthwest$linear.predictor)
+    
+    mu.gam.fme <- predict(eta.finmarkeast,newdata=test.finmarkeast.d,type="link")
+    sigma.gam.fme <- sd(log(eta.finmarkeast$y) - eta.finmarkeast$linear.predictor)
     
     oos.predictions <- rbind(oos.predictions,
                              data.table(
-                               eta = predict.gam(eta.auto,
-                                                 newdata=test.gamdat.d,
+                               eta = predict.gam(eta.midsouthwest,
+                                                 newdata=test.midsouthwest.d,
                                                  type="response"),
-                               eta.obs = test.gamdat.d[,get("qind")],
-                               mu.gam = mu.gam, sigma.gam = sigma.gam,
-                               model = rep("auto",n),fold=rep(i,n),d=rep(di,n),
-                               ID = test.gamdat.d[,get("ID")]))
+                               eta.obs = test.midsouthwest.d[,get("qind")],
+                               mu.gam = mu.gam.msw, sigma.gam = sigma.gam.msw,
+                               model = rep("midsouthwest",n.msw),fold=rep(i,n.msw),d=rep(di,n.msw),
+                               ID = test.midsouthwest.d[,get("ID")]))
+    
+    oos.predictions <- rbind(oos.predictions,
+                             data.table(
+                               eta = predict.gam(eta.finmarkeast,
+                                                 newdata=test.finmarkeast.d,
+                                                 type="response"),
+                               eta.obs = test.finmarkeast.d[,get("qind")],
+                               mu.gam = mu.gam.fme, sigma.gam = sigma.gam.fme,
+                               model = rep("finmarkeast",n.fme),fold=rep(i,n.fme),d=rep(di,n.fme),
+                               ID = test.finmarkeast.d[,get("ID")]))
+    
     
 
     
@@ -387,12 +387,7 @@ for(di in unique(gfam[,get("d")])){
 }
 
 
-op1 <- readRDS(paste0("~/floodGAM/results/output/median-(index-flood)/",
-                      "gamfelt_hydagsupp_median_flood_oos_pred.rds"))
-
-oos.pred <- rbind(oos.predictions,op1[model!="auto"])
-
-saveRDS(oos.pred,
+saveRDS(oos.predictions,
         file = paste0("~/floodGAM/results/output/median-(index-flood)/",
-                      "gamfelt_hydagsupp_median_flood_oos_pred.rds"))
+                      "regions_median_flood_oos_pred.rds"))
 
